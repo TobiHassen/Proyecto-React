ef55754cef61e52226985dfa3333419684932722
tobias hassen <tobiashassencoder@gmail.com>
2023-06-16 13:22:31 -0300

    agregue firabase y optimizacion para el proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
+import Contacto from "./components/Contacto";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import Navbar from "./components/Navbar";
 import Nosotros from "./components/Nosotros";
-import "./main.css"
+import "./main.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import Carrito from "./components/Carrito";
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <div>
+      <CartProvider>
+        <BrowserRouter>
 
-      <Navbar />
+          <Navbar />
 
-      <Routes>
-        <Route path="/" element= {<ItemListContainer />} />
-        <Route path="/item/:id" element= {<ItemDetailContainer />}/>
-        <Route path="/productos/:categoria" element= {<ItemListContainer />} />
-        <Route path="/nosotros" element= {<Nosotros />}/>
-      </Routes>
-
-    </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/item/:id" element={<ItemDetailContainer />}/>
+            <Route path="/productos" element={<ItemListContainer />} />
+            <Route path="/productos/:categoria" element={<ItemListContainer />} />
+            <Route path="/nosotros" element={<Nosotros />}/>
+            <Route path="/contacto" element={<Contacto />}/>
+            <Route path="/carrito" element={<Carrito />}/>
+          </Routes>
+          
+        </BrowserRouter>
+      </CartProvider>
+    </div>
   );
 }
 
